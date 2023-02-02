45ab3b37e2fa0cea8d57496f3f548e1cecb3c98b
Amirbahador Moineddini <abmoineddini@gmail.com>
2023-02-02 23:08:42 +0000

    New launch file for the real robot

--- launch/joystick_sim.launch.py
+++ launch/joystick_sim.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='true',
             description='Use sim time if true'),
         joy_node,
         teleop_node, 
